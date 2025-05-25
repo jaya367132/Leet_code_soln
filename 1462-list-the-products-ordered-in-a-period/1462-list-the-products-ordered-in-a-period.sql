49f26666fe6909684f941e4841be4647462a2176
Jaya sinha <89565049+jaya367132@users.noreply.github.com>
2025-05-25 12:25:25 +0530

    Time: 650 ms (97.82%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -6,5 +6,5 @@ using(product_id)
 where o.order_date >= '2020-02-01' AND o.order_date < '2020-03-01')
 
 select product_name , sum(unit) as unit from cte 
-group by product_id , product_id
+group by product_id 
 HAVING SUM(unit) >= 100
